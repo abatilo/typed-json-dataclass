c5a37b7bbe89aaacef55bf62db2a38562697fc22
Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>
2019-06-19 21:44:42 +0200

    Add JSON tests, rename some tests for clarity

--- tests/test_issue_8.py
+++ tests/test_issue_8.py
@@ -48,7 +48,16 @@ def test_that_dataclass_with_init_var_to_dict_leads_to_warning() -> None:
     assert result == {'a': 3, 'b': 'f'}
 
 
-def test_init_var_dc_no_warnings_when_acknowledged() -> None:
+def test_that_dataclass_with_init_var_to_json_leads_to_warning() -> None:
+    dcls = DataclassWithInitVar('foo')
+
+    with pytest.warns(UserWarning, match='init-only variables'):
+        result = dcls.to_json()
+
+    assert result == '{"a": 3, "b": "f"}'
+
+
+def test_init_var_dc_to_dict_no_warnings_when_acknowledged() -> None:
     dcls = DataclassWithInitVar('foo')
 
     with pytest.warns(None) as recorded_warnings:
@@ -58,7 +67,17 @@ def test_init_var_dc_no_warnings_when_acknowledged() -> None:
     assert result == {'a': 3, 'b': 'f'}
 
 
-def test_init_var_dc_no_warnings_when_init_var_has_default_value() -> None:
+def test_init_var_dc_to_json_no_warnings_when_acknowledged() -> None:
+    dcls = DataclassWithInitVar('foo')
+
+    with pytest.warns(None) as recorded_warnings:
+        result = dcls.to_json(warn_on_initvar=False)
+
+    assert not recorded_warnings
+    assert result == '{"a": 3, "b": "f"}'
+
+
+def test_init_var_dc_to_dict_no_warning_when_default_value_provided() -> None:
     dcls = DataclassWithDefaultInitVar('foo')
 
     with pytest.warns(None) as recorded_warnings:
@@ -68,6 +87,16 @@ def test_init_var_dc_no_warnings_when_init_var_has_default_value() -> None:
     assert result == {'a': 3, 'b': 'f'}
 
 
+def test_init_var_dc_to_json_no_warning_when_default_value_provided() -> None:
+    dcls = DataclassWithDefaultInitVar('foo')
+
+    with pytest.warns(None) as recorded_warnings:
+        result = dcls.to_json()
+
+    assert not recorded_warnings
+    assert result == '{"a": 3, "b": "f"}'
+
+
 def test_that_dataclass_with_init_var_from_dict_leads_to_typeerror() -> None:
     raw_dict = {'a': 3, 'b': 'f'}
 
@@ -75,10 +104,26 @@ def test_that_dataclass_with_init_var_from_dict_leads_to_typeerror() -> None:
         DataclassWithInitVar.from_dict(raw_dict)
 
 
+def test_that_dataclass_with_init_var_from_json_leads_to_typeerror() -> None:
+    raw_json = '{"a": 3, "b": "f"}'
+
+    with pytest.raises(TypeError, match='init-only variables'):
+        DataclassWithInitVar.from_json(raw_json)
+
+
 def test_init_var_dc_from_dict_no_error_when_default_value_provided() -> None:
     raw_dict = {'a': 3, 'b': 'f'}
 
     result = DataclassWithDefaultInitVar.from_dict(raw_dict)
+
+    assert result.a == 3
+    assert result.b == 'f'
+
+
+def test_init_var_dc_from_json_no_error_when_default_value_provided() -> None:
+    raw_json = '{"a": 3, "b": "f"}'
+
+    result = DataclassWithDefaultInitVar.from_json(raw_json)
 
     assert result.a == 3
     assert result.b == 'f'
